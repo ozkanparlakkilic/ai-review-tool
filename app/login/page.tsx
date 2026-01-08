1775535de444a759ffc98562c8c47222d7126cef
ozkanparlakkilic <prlkklc@gmail.com>
2026-01-08 09:37:25 +0300

    feat(auth): implement authentication pages

---
+++ app/login/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { Suspense } from "react";
+import Link from "next/link";
+import { LoginForm } from "@/features/auth/components/login-form";
+import { Skeleton } from "@/components/ui/skeleton";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { AuthLayout } from "@/features/auth/components/auth-layout";
+
+export default function LoginPage() {
+  return (
+    <AuthLayout>
+      <Card className="gap-4">
+        <CardHeader>
+          <CardTitle className="text-lg tracking-tight">Sign in</CardTitle>
+          <CardDescription>
+            Enter your email and password below to log into your account. <br />
+            Don't have an account?{" "}
+            <Link
+              href="/sign-up"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Sign up
+            </Link>
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Suspense fallback={<Skeleton className="h-[300px] w-full" />}>
+            <LoginForm />
+          </Suspense>
+        </CardContent>
+        <CardFooter>
+          <p className="px-8 text-center text-sm text-muted-foreground">
+            By clicking sign in, you agree to our{" "}
+            <a
+              href="/terms"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Terms of Service
+            </a>{" "}
+            and{" "}
+            <a
+              href="/privacy"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Privacy Policy
+            </a>
+            .
+          </p>
+        </CardFooter>
+      </Card>
+    </AuthLayout>
+  );
+}
---
+++ app/otp/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { AuthLayout } from "@/features/auth/components/auth-layout";
+import { OtpForm } from "@/features/auth/components/otp-form";
+
+export default function OtpPage() {
+  return (
+    <AuthLayout>
+      <Card className="gap-4">
+        <CardHeader>
+          <CardTitle className="text-base tracking-tight">
+            Two-factor Authentication
+          </CardTitle>
+          <CardDescription>
+            Please enter the authentication code. <br /> We have sent the
+            authentication code to your email.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <OtpForm />
+        </CardContent>
+        <CardFooter>
+          <p className="px-8 text-center text-sm text-muted-foreground">
+            Haven't received it?{" "}
+            <Link
+              href="/login"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Resend a new code.
+            </Link>
+            .
+          </p>
+        </CardFooter>
+      </Card>
+    </AuthLayout>
+  );
+}
